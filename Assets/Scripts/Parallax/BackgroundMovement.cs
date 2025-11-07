c76550d17dc4e0fd9c7d6a010aae01dfa9e0ea4e
SergioVegas <sergio.vegas.7e8@itb.cat>
2025-11-07 17:45:50 +0100

    Laser prop

--- Assets/Scripts/Parallax/BackgroundMovement.cs
+++ Assets/Scripts/Parallax/BackgroundMovement.cs
@@ -2,21 +2,21 @@ using UnityEngine;
 
 public class BackgroundMovement : MonoBehaviour
 {
-    private float startPosition, length ;
+    private float startPosition;
+    private float length = 29.0659f;
     public GameObject camera;
     public float parallaxEffect;
 
     private void Start()
     {
         startPosition = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        //length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
     private void FixedUpdate()
     {
         float distance = camera.transform.position.x * parallaxEffect; // 0 = seguirá a la camara || 1= no se mueve
         float movement = camera.transform.position.x * (1 - parallaxEffect);
         transform.position = new Vector3(startPosition + distance, transform.position.y, transform.position.z);
-
         if (movement > startPosition + length)
         {
             startPosition += length;
