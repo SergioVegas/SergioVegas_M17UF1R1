a3899712962f68dea597c2ea1d66a1bf362b9173
SergioVegas <sergio.vegas.7e8@itb.cat>
2025-11-20 23:17:37 +0100

    Parallax del cielo arreglado

--- Assets/Scripts/Parallax/BackgroundMovement.cs
+++ Assets/Scripts/Parallax/BackgroundMovement.cs
@@ -4,18 +4,17 @@ public class BackgroundMovement : MonoBehaviour
 {
     private float startPosition;
     private float length = 29.0659f;
-    public GameObject camera;
+    public GameObject _camera;
     public float parallaxEffect;
 
     private void Start()
     {
         startPosition = transform.position.x;
-        //length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
-    private void FixedUpdate()
+    private void LateUpdate()
     {
-        float distance = camera.transform.position.x * parallaxEffect; // 0 = seguirá a la camara || 1= no se mueve
-        float movement = camera.transform.position.x * (1 - parallaxEffect);
+        float distance = _camera.transform.position.x * parallaxEffect; // 0 = seguirá a la camara || 1= no se mueve
+        float movement = _camera.transform.position.x * (1 - parallaxEffect);
         transform.position = new Vector3(startPosition + distance, transform.position.y, transform.position.z);
         if (movement > startPosition + length)
         {
